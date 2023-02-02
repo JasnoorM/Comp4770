93d90a289527879caf692832d040c15993c14227
sameeksha-nair <sameeksha.canada@gmail.com>
2023-02-02 12:03:28 -0500

    Update CleanerActuator.cs

--- Easy-AI-Template/Assets/Scripts/A1/Actuators/CleanerActuator.cs
+++ Easy-AI-Template/Assets/Scripts/A1/Actuators/CleanerActuator.cs
@@ -10,19 +10,26 @@ public class CleanerActuator : Actuator
 
     public override bool Act(object agentAction)
     {
-        Floor tile = (Floor)agentAction;
-
-        
-        //Agent.transform.Translate(tile.transform.position * Time.deltaTime);
-        Agent.transform.position = Vector3.MoveTowards(transform.position, tile.transform.position, Time.deltaTime *5f);
-
-        if (Vector3.Distance(Agent.transform.position, tile.transform.position) < 0.1f)
+        if (agentAction == null)
         {
+            Agent.transform.position = Vector3.MoveTowards(transform.position, new Vector3(0.0f, 0.0f, 0.0f), Time.deltaTime * 5f);
+        }
+        else
+        {
+            Floor tile = (Floor)agentAction;
 
-            tile.Clean();
-            
+
+            //Agent.transform.Translate(tile.transform.position * Time.deltaTime);
+            Agent.transform.position = Vector3.MoveTowards(transform.position, tile.transform.position, Time.deltaTime * 5f);
+
+            if (Vector3.Distance(Agent.transform.position, tile.transform.position) < 0.1f)
+            {
+
+                tile.Clean();
 
 
+
+            }
         }
         
         return true;
