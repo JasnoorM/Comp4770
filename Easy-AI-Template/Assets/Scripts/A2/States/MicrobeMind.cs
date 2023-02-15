52da44817a898d86d88dbcbeb9bbfd0ee8987f4c
JasnoorM <mallhij@uwindsor.ca>
2023-02-14 19:52:02 -0500

    yeet

--- Easy-AI-Template/Assets/Scripts/A2/States/MicrobeMind.cs
+++ Easy-AI-Template/Assets/Scripts/A2/States/MicrobeMind.cs
@@ -9,9 +9,16 @@ namespace A2.States
     [CreateAssetMenu(menuName = "A2/States/Microbe Mind", fileName = "Microbe Mind")]
     public class MicrobeMind : State
     {
+        //A2.Sensors.NearestPickupSensor sensor;
+        MicrobeSeekingPickupState seek;
+
         public override void Execute(Agent agent)
         {
             // TODO - Assignment 2 - Complete the mind of the microbes.
+            seek.Enter(agent);
+            seek.Execute(agent);
+
+            
         }
     }
 }
--- Easy-AI-Template/Assets/Scripts/A2/States/MicrobeSeekingPickupState.cs
+++ Easy-AI-Template/Assets/Scripts/A2/States/MicrobeSeekingPickupState.cs
@@ -1,5 +1,8 @@
-﻿using EasyAI;
+﻿using A1;
+using A2.Pickups;
+using EasyAI;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace A2.States
 {
@@ -9,14 +12,24 @@ namespace A2.States
     [CreateAssetMenu(menuName = "A2/States/Microbe Seeking Pickup State", fileName = "Microbe Seeking Pickup State")]
     public class MicrobeSeekingPickupState : State
     {
+        A2.Sensors.NearestPickupSensor sensor;
+        MicrobeBasePickup pickup;
+
+
+
         public override void Enter(Agent agent)
         {
             // TODO - Assignment 2 - Complete this state. Have microbes look for pickups.
+            pickup = (MicrobeBasePickup)sensor.Sense();
         }
         
         public override void Execute(Agent agent)
         {
             // TODO - Assignment 2 - Complete this state. Have microbes look for pickups.
+
+
+            //pickup.transform.position = Vector3.MoveTowards(transform.position, pickup.transform.position, Time.deltaTime * 5f);
+            agent.Move(pickup.transform.position *Time.deltaTime *1f);
         }
         
         public override void Exit(Agent agent)
