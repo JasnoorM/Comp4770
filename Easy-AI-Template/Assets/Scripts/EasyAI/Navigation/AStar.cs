18b5181b1d5b4365e409d0740bac0f5b74ccdae6
sameeksha-nair <sameeksha.canada@gmail.com>
2023-03-30 20:47:34 -0400

    Update AStar.cs

--- Easy-AI-Template/Assets/Scripts/EasyAI/Navigation/AStar.cs
+++ Easy-AI-Template/Assets/Scripts/EasyAI/Navigation/AStar.cs
@@ -17,35 +17,18 @@ namespace EasyAI.Navigation
         /// <param name="connections">All node connections in the scene.</param>
         /// <returns>The path of nodes to take to get from the starting position to the ending position.</returns>
         /// 
-
         
+
         public static List<Vector3> Perform(Vector3 current, Vector3 goal, List<Connection> connections)
         {
+            
             AStarNode node;
+            
             foreach (Connection i in connections)
             {
                 node = new AStarNode(i.A, goal);
             }
-            /*AStarNode node = new AStarNode();
-            float i = node.CostF;*/
-
-
-            //look at AStarNode
-            //assign lookup table to manager
-            //Navigate agent to position of node
-
-            //open node
-
-            if (current == goal)
-            {
-                Debug.Log("Reached Goal");
-            }else
-            {
-               
-
-            }
-
-
+            
             // TODO - Assignment 4 - Implement A* pathfinding.
             return new();
         }
