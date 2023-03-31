4fccce29091d91037c2a502b6e3fe484b933f8c2
JasnoorM <mallhij@uwindsor.ca>
2023-03-30 20:21:05 -0400

    yee

--- Easy-AI-Template/Assets/Scripts/EasyAI/Navigation/Generators/CornerGraphGenerator.cs
+++ Easy-AI-Template/Assets/Scripts/EasyAI/Navigation/Generators/CornerGraphGenerator.cs
@@ -24,13 +24,14 @@ namespace EasyAI.Navigation.Generators
             
             int check = 0;
 
-            for (int i = 0; i < NodeArea.RangeX; i++)
+            for (int i = 0; i < NodeArea.RangeX; i++) //goes through all ranges of x
             {
 
-                for (int j = 0; j < NodeArea.RangeZ; j++)
+                for (int j = 0; j < NodeArea.RangeZ; j++) //goes through all ranges of z
                 {
-                    if (NodeArea.IsOpen(i,j))
+                    if (NodeArea.IsOpen(i,j)) 
                     {
+                        //checking for corners
                         if (!NodeArea.IsOpen(i - 1, j - 1)) check++;
                         if (!NodeArea.IsOpen(i - 1, j)) check++;
                         if (!NodeArea.IsOpen(i, j - 1)) check++;
@@ -42,6 +43,7 @@ namespace EasyAI.Navigation.Generators
                         Debug.Log(check);
                         if (check == 1)
                         {
+                            //add node
                             if(NodeArea.IsOpen(i + cornerNodeSteps, j + cornerNodeSteps)) NodeArea.AddNode(i + cornerNodeSteps, j + cornerNodeSteps);
                             if (NodeArea.IsOpen(i - cornerNodeSteps, j - cornerNodeSteps))NodeArea.AddNode(i - cornerNodeSteps, j - cornerNodeSteps);
                         }
@@ -58,22 +60,6 @@ namespace EasyAI.Navigation.Generators
                 
 
             }
-
-
-            //40 nodes, 236 connections, 1560 lookups
-
-            //calc nodes per step(scans that need to be free from a corner)
-            //4 nodes per step with 3 corner node steps
-
-            //to loop through all spaces:
-            //NodeArea.RangeX and NodeArea.RangeZ
-            //double loop
-
-            //check to see if space is open:
-            //NodeArea.IsOpen(int x, int z)
-
-
-            //Place node: NodeArea.Place(int x, int y)
 
 
 
